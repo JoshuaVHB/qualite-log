153fbc5e9b7174a5d3db71b06eb547b644b36a23
Albin Calais <albincalais@gmail.com>
2022-12-23 16:56:27 +0100

    feat: better ssr^2

--- Server/src/main/java/reserve/view/front/TkSpecificResource.java
+++ Server/src/main/java/reserve/view/front/TkSpecificResource.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.takes.Request;
 import org.takes.Response;
@@ -13,11 +15,16 @@ import org.takes.facets.auth.Identity;
 import org.takes.facets.auth.RqAuth;
 import org.takes.rs.RsWithBody;
 
+import reserve.Main;
+import reserve.util.Logger;
 import reserve.view.WebServer;
 
 public class TkSpecificResource implements Take {
 	
 	private static final boolean USE_CACHE = false;
+	private static final Pattern SSR_CONDITION_PATTERN = Pattern.compile("\\{\\{(.*?)\\}\\}"); // server-side rendering condition: {{condition}}
+	
+	private static final Logger logger = Main.LOGGER_FACTORY.getLogger("web-files");
 	
 	private final String path;
 	private final Map<String, String> resources = new HashMap<>();
@@ -27,12 +34,45 @@ public class TkSpecificResource implements Take {
 	}
 	
 	public Response act(Request req) throws Exception {
-		Identity identity = new RqAuth(req).identity();
+		
+		/*
+		 * Java really isn't designed for dynamic web page composition,
+		 * this is the best I could do to emulate server-side rendering.
+		 * Next time I'll use php or js, java is good as a back-end
+		 * language but not practical enough.
+		 */
+		StringBuilder sb = new StringBuilder(loadFile(path));
+		int searchIndex = sb.length();
+		while((searchIndex = sb.lastIndexOf("<!--", searchIndex-1)) != -1) {
+			int newLine = sb.indexOf("\n", searchIndex);
+			String condPart = sb.substring(searchIndex+4, newLine).strip();
+			Matcher m = SSR_CONDITION_PATTERN.matcher(condPart);
+			int commentEnd = sb.indexOf("-->", searchIndex);
+			if(!m.matches())
+				continue;
+			String condition = m.group(1);
+			if(isConditionRespected(condition, req)) {
+//				String body = sb.substring(searchIndex+4+m.end(0), commentEnd);
+//				sb.replace(searchIndex, commentEnd+3, body);
+				sb.delete(commentEnd, commentEnd+3);            // remove "-->"
+				sb.delete(searchIndex, searchIndex+4+m.end(0)); // remove "<!--{{...}}"
+			} else {
+				sb.delete(searchIndex, commentEnd+3); // remove the whole block
+			}
+		}
+		
+		return new RsWithBody(sb.toString());
+	}
+	
+	private boolean isConditionRespected(String condition, Request request) throws IOException {
+		Identity identity = new RqAuth(request).identity();
 		boolean authentified = identity != Identity.ANONYMOUS;
 		
-		String text = loadFile(path);
-		text = text.replaceAll("\\$\\$LOGGED\\$\\$", authentified ? loadFile("/parts/_logged.html") : loadFile("/parts/_not_logged.html"));
-		return new RsWithBody(text);
+		switch(condition) {
+		case "LOGGED":     return authentified;
+		case "NOT_LOGGED": return !authentified;
+		default: logger.warn("Unknown condition '" + condition + "'"); return false;
+		}
 	}
 	
 	private String loadFile(String path) {
