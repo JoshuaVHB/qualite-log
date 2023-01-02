ec3888657f1e7c6d379d6efdeb0864eb4c36bd00
Albin Calais <albincalais@gmail.com>
2023-01-02 18:19:11 +0100

    feat: user modification permissions

--- Server/src/main/java/reserve/view/entry/TkEditUser.java
+++ Server/src/main/java/reserve/view/entry/TkEditUser.java
@@ -26,7 +26,7 @@ public class TkEditUser implements Take {
 		User editorUser = FormUtils.getUserIdentity(req);
 		RqForm form = new RqFormBase(req);
 
-		if(editorUser == null || !editorUser.isAdmin())
+		if(editorUser == null)
 			return new RsWithStatus(HttpURLConnection.HTTP_UNAUTHORIZED);
 		
 		String editedUserId = FormUtils.getParamString(form, "id", UserController.USER_ID_FORMAT, true);
@@ -34,7 +34,9 @@ public class TkEditUser implements Take {
 		if(FormUtils.getParamBool(form, "delete", true) != null) {
 			// remove user
 			User user = users.getById(editedUserId);
-			if(user == null)
+			if(!editorUser.isAdmin())
+				return new RsWithStatus(HttpURLConnection.HTTP_UNAUTHORIZED);
+			if(user == null || editorUser.getId().equals(editedUserId))
 				return new RsWithStatus(new RsText("Unknown user"), HttpURLConnection.HTTP_BAD_REQUEST);
 			users.removeUser(user);
 			return new RsWithStatus(HttpURLConnection.HTTP_OK);
@@ -48,6 +50,9 @@ public class TkEditUser implements Take {
 		String mail = FormUtils.getParamString(form, "mail", UserController.USER_MAIL_FORMAT, false);
 		
 		if(editedUserId == null) {
+			if(!editorUser.isAdmin())
+				return new RsWithStatus(new RsText("You must be an administrator"), HttpURLConnection.HTTP_UNAUTHORIZED);
+				
 			// create a new user
 			String password = FormUtils.getParamString(form, "password", UserController.USER_PASSWORD_FORMAT, false);
 			String newId = users.getNextUserId();
@@ -59,9 +64,13 @@ public class TkEditUser implements Take {
 				return new RsWithStatus(new RsText(e.getMessage()), HttpURLConnection.HTTP_BAD_REQUEST);
 			}
 		} else {
+			if((!editedUserId.equals(editorUser.getId()) && !editorUser.isAdmin()) ||
+				(!editorUser.isAdmin() && isAdmin))
+				return new RsWithStatus(new RsText("You must be an administrator"), HttpURLConnection.HTTP_UNAUTHORIZED);
+			
 			// edit the user
 			User user = users.getById(editedUserId);
-			if(editedUserId.equals(editorUser.getId()) && !isAdmin)
+			if(editedUserId.equals(editorUser.getId()) && editorUser.isAdmin() && !isAdmin)
 				return new RsWithStatus(new RsText("You cannot disown yourself of admin privileges"), HttpURLConnection.HTTP_BAD_REQUEST);
 			user.setEmail(mail);
 			user.setFirstName(firstName);
